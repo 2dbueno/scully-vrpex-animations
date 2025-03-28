5aed7a21c0d846ae636e9cba5c4af0d35974c537
2dbueno <eduardonunesbueno56@gmail.com>
2025-03-28 20:22:02 -0300

    feat: adicionados todas as animações para o referido arquivo

--- animations/scenarios.lua
+++ animations/scenarios.lua
@@ -1,5 +1,115 @@
 return {
     {
+        Label = 'ATM',
+        Command = 'atm',
+        Scenario = 'PROP_HUMAN_ATM',
+    },
+    {
+        Label = 'Vasculhar Lixo',
+        Command = 'mecherlixo',
+        Scenario = 'PROP_HUMAN_BUM_BIN',
+    },
+    {
+        Label = 'Binoculars',
+        Command = 'binoculos',
+        Scenario = 'WORLD_HUMAN_BINOCULARS',
+    },
+    {
+        Label = 'Dormir na Rua',
+        Command = 'dormirrua',
+        Scenario = 'WORLD_HUMAN_BUM_SLUMPED',
+    },
+    {
+        Label = 'Aplaudir',
+        Command = 'aplaudir',
+        Scenario = 'WORLD_HUMAN_CHEERING',
+    },
+    {
+        Label = 'Barra Fixa',
+        Command = 'barrafixa',
+        Scenario = 'PROP_HUMAN_MUSCLE_CHIN_UPS',
+    },
+    {
+        Label = 'Prancheta 2',
+        Command = 'prancheta2',
+        Scenario = 'WORLD_HUMAN_CLIPBOARD',
+    },
+    {
+        Label = 'Policial Ocioso',
+        Command = 'policialocioso',
+        Scenario = 'WORLD_HUMAN_COP_IDLES',
+    },
+    {
+        Label = 'Furar',
+        Command = 'furar',
+        Scenario = 'WORLD_HUMAN_CONST_DRILL',
+    },
+    {
+        Label = 'Filmar Chocante',
+        Command = 'filmarchocante',
+        Scenario = 'WORLD_HUMAN_MOBILE_FILM_SHOCKING',
+    },
+    {
+        Label = 'Flexionar',
+        Command = 'flexionar',
+        Scenario = 'WORLD_HUMAN_MUSCLE_FLEX',
+    },
+    {
+        Label = 'Jardinar',
+        Command = 'jardinar',
+        Scenario = 'WORLD_HUMAN_GARDENER_PLANT',
+    },
+    {
+        Label = 'Guarda',
+        Command = 'guarda',
+        Scenario = 'WORLD_HUMAN_GUARD_STAND',
+    },
+    {
+        Label = 'Martelar',
+        Command = 'martelar',
+        Scenario = 'WORLD_HUMAN_HAMMERING',
+    },
+    {
+        Label = 'Socializar',
+        Command = 'socializar',
+        Scenario = 'WORLD_HUMAN_HANG_OUT_STREET',
+    },
+    {
+        Label = 'Impetuoso',
+        Command = 'impetuoso',
+        Scenario = 'WORLD_HUMAN_STAND_IMPATIENT',
+    },
+    {
+        Label = 'Zelador',
+        Command = 'zelador',
+        Scenario = 'WORLD_HUMAN_JANITOR',
+    },
+    {
+        Label = 'Correr',
+        Command = 'correr',
+        Scenario = 'WORLD_HUMAN_JOG_STANDING',
+    },
+    {
+        Label = 'Ajoelhar',
+        Command = 'ajoelhar',
+        Scenario = 'CODE_HUMAN_MEDIC_KNEEL',
+    },
+    {
+        Label = 'Encostar',
+        Command = 'encostar',
+        Scenario = 'WORLD_HUMAN_LEANING',
+    },
+    {
+        Label = 'Carrinho de Compras',
+        Command = 'carrinhocompras',
+        Scenario = 'PROP_HUMAN_BUM_SHOPPING_CART',
+    },
+    {
+        Label = 'Olhar',
+        Command = 'olhar',
+        Scenario = 'CODE_HUMAN_CROSS_ROAD_WAIT',
+    },
+    {
         Label = "pensativo",
         Command = "pensativo",
         Animation = "WORLD_HUMAN_PROSTITUTE_LOW_CLASS",
@@ -52,5 +162,85 @@ return {
         Command = "tragar",
         Animation = "WORLD_HUMAN_DRUG_DEALER",
         Dictionary = "",
+    },
+    {
+        Label = 'Passar pano',
+        Command = 'pano',
+        Scenario = 'WORLD_HUMAN_MAID_CLEAN',
+    },
+    {
+        Label = 'Médico',
+        Command = 'medico',
+        Scenario = 'CODE_HUMAN_MEDIC_TEND_TO_DEAD',
+    },
+    {
+        Label = 'Músico',
+        Command = 'musico',
+        Scenario = 'WORLD_HUMAN_MUSICIAN',
+    },
+    {
+        Label = 'Bloco de Notas 2',
+        Command = 'anotar',
+        Scenario = 'CODE_HUMAN_MEDIC_TIME_OF_DEATH',
+    },
+    {
+        Label = 'Parquímetro',
+        Command = 'parquimetro',
+        Scenario = 'PROP_HUMAN_PARKING_METER',
+    },
+    {
+        Label = 'Sentar',
+        Command = 'sentar',
+        Scenario = 'PROP_HUMAN_SEAT_CHAIR_MP_PLAYER',
+    },
+    {
+        Label = 'Sentar 2',
+        Command = 'sentar2',
+        Scenario = 'PROP_HUMAN_SEAT_CHAIR_DRINK',
+    },
+    {
+        Label = 'Sentar 3',
+        Command = 'sentar3',
+        Scenario = 'PROP_HUMAN_SEAT_CHAIR_DRINK_BEER',
+    },
+    {
+        Label = 'Sentar 4',
+        Command = 'sentar4',
+        Scenario = 'PROP_HUMAN_SEAT_CHAIR_FOOD',
+    },
+    {
+        Label = 'Sentar 5',
+        Command = 'sentar5',
+        Scenario = 'PROP_HUMAN_SEAT_STRIP_WATCH',
+    },
+    {
+        Label = 'Estátua',
+        Command = 'estatua',
+        Scenario = 'WORLD_HUMAN_HUMAN_STATUE',
+    },
+    {
+        Label = 'Tomar Sol 3',
+        Command = 'tomarsol3',
+        Scenario = 'WORLD_HUMAN_SUNBATHE',
+    },
+    {
+        Label = 'Tomar Sol de Costas',
+        Command = 'tomarsol4',
+        Scenario = 'WORLD_HUMAN_SUNBATHE_BACK',
+    },
+    {
+        Label = 'Mandar Mensagem',
+        Command = 'sms',
+        Scenario = 'WORLD_HUMAN_STAND_MOBILE',
+    },
+    {
+        Label = 'Encarar',
+        Command = 'encarar',
+        Scenario = 'WORLD_HUMAN_WINDOW_SHOP_BROWSE',
+    },
+    {
+        Label = 'Yoga',
+        Command = 'yoga',
+        Scenario = 'WORLD_HUMAN_YOGA',
     },
 }
